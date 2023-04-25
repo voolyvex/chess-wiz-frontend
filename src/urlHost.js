55f8f5478316c23b1ab6615a5db9321c38224f8a
voolyvex <1joeknapp@gmail.com>
2023-04-24 23:56:48 -0500

    fix: change url path for deployment with cloudflare proxy

--- src/urlHost.js
+++ src/urlHost.js
@@ -1,4 +1,4 @@
 export const URL_HOST =
   process.env.NODE_ENV === "production"
-    ? `https://amplify-eb-proxy.1joeknapp4809.workers.dev/http://backend-rvl-env.eba-ejsmgqie.us-east-1.elasticbeanstalk.com`
+    ? `https://amplify-eb-proxy.1joeknapp4809.workers.dev/?targetUrl=http://chesswizbackend-env.eba-yh9fjdhr.us-east-1.elasticbeanstalk.com`
     : `http://127.0.0.1:8000`;
