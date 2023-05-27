04d860fe06f78a49bb645c14e657d71bad362fcf
voolyvex <1joeknapp@gmail.com>
2023-05-27 18:23:27 -0500

    added a fallback path to webpack.config.js for path-browserify to fix conflict between stockfish.js and webpack

--- src/components/Engine/Engine.jsx
+++ src/components/Engine/Engine.jsx
@@ -1,25 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import Stockfish from 'stockfish';
+import React, { useState, useEffect, useRef } from 'react';
+import Stockfish from '../../lib/stockfish.js';
+
+
 
 const Engine = ({ position }) => {
   const [evaluation, setEvaluation] = useState(null);
-  const engine = new Stockfish();
+  const stockfishRef = useRef(null);
 
   useEffect(() => {
-    engine.postMessage('uci');
-  }, []);
+    // Create an instance of the Stockfish engine
+    const stockfish = new Stockfish();
+    stockfishRef.current = stockfish;
 
-  useEffect(() => {
-    engine.postMessage(`position fen ${position}`);
-    engine.postMessage('go depth 15');
+    // Add a message listener to receive messages from the engine
+    const handleMessage = (message) => {
+      console.log(message);
 
-    engine.onmessage = (event) => {
-      const message = event.data;
+      // Send a position and search command to the engine when it's ready
+      if (message === 'ready') {
+        stockfish.postMessage(`position fen ${position}`);
+        stockfish.postMessage('go depth 15');
+      }
+
+      // Parse the evaluation score and update the state
       if (message.startsWith('info depth ')) {
         const match = message.match(/score cp (-?\d+) /);
         const score = match ? parseInt(match[1], 10) / 100.0 : null;
         setEvaluation(score);
       }
+    };
+    stockfish.addEventListener('message', handleMessage);
+
+    // Start the engine by sending the "uci" command
+    stockfish.postMessage('uci');
+
+    // Cleanup function to terminate the engine
+    return () => {
+      stockfish.removeEventListener('message', handleMessage);
+      stockfish.postMessage('quit');
+      stockfish.terminate();
     };
   }, [position]);
 
